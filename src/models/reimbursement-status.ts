22995706be28c86cf3f58187b86a3f36d465cd21
Dane Vosmeier <dane.vosmeier@revature.net>
2019-11-26 14:56:29 -0500

    nuked it

--- src/models/reimbursement-status.ts
+++
@@ -1,9 +0,0 @@
-export class ReimbursementStatus{
-    statusId: number
-    status: string
-
-    constructor(statusId:number, status:string){
-        this.statusId = statusId
-        this.status = status
-    }
-}
