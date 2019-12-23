c12d3a8b41b032acd12e78bc125c8061381178d2
Dane Vosmeier <dane.vosmeier@revature.net>
2019-12-22 19:37:46 -0500

    cors fixed

--- src/middleware.ts/cors-middleware.ts
+++ src/middleware.ts/cors-middleware.ts
@@ -2,10 +2,12 @@ import { Request } from 'express';
 
 
 export function corsFilter(req: Request, res, next) {
-    res.header('Access-Control-Allow-Origin', `${req.headers.origin}` );
-    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+    res.header('Access-Control-Allow-Origin', `${req.headers.origin}`);
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept,');
     res.header('Access-Control-Allow-Credentials', 'true');
-
+    res.header('Access-Control-Expose-Headers', 'token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH');
+    
     if(req.method === 'OPTIONS') {
         res.status(200).send()
     } else {
